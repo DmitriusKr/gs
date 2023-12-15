03a07d8e068bfcb8d711ab995d3f7a8140266e0c
Dmitrius.Kr <dmitrius.kr@gmail.com>
2023-12-15 14:24:15 +0500

    initial

---
+++ js/script.js
@@ -0,0 +1,43 @@
+﻿const PORT = 9999; 
+const socket = new WebSocket(`ws://localhost:${PORT}`);
+
+const generateButton = document.getElementById("generateButton");
+const fileContent = document.getElementById("fileContent");
+const lamp = document.getElementById("lamp");
+
+socket.onopen = function(e) {
+  console.log("open"); 
+  socket.send("check");
+};
+
+socket.onmessage = function(event) {
+  console.log(event);
+  data = JSON.parse(event.data);
+  var status = data.status;
+  if (status === '1') {
+    lamp.innerHTML = ("<img src = './assets/on.png'/>");
+    generateButton.innerHTML = "Выключить";
+  }
+  else {
+    lamp.innerHTML = ("<img src = './assets/off.png'/>");
+    generateButton.innerHTML = "Включить";
+  }
+  if (data.time) {
+    fileContent.innerHTML =' Статус включения: '+ data.status+ '<br>Время чтения файла на сервере '+ data.time + ' ms'+'<br>Размер файла: '+data.size +' bytes'+ '<br>coдeржимое файла\n' + data.content;
+  }
+  else {
+    fileContent.innerHTML = 'Статус включения: '+ data.status;
+  }
+};
+generateButton.addEventListener("click", () => {
+  socket.send("toggle");
+});   
+socket.addEventListener("close", (event) => {
+    if (event.wasClean) {
+      fileContent.textContent = "Соединение закрыто чисто";
+    } else {
+      fileContent.textContent = "Обрыв соединения";
+    }
+    fileContent.textContent +=
+      "\n Код: " + event.code + " причина: " + event.reason;
+  });
---
+++ server/server.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const WebSocket = require('ws');
+const fs = require("fs");
+
+const PORT = 9999;
+
+const app = express();
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+wss.on("connection", (ws) => {
+    console.log('someone connected');
+    ws.on("error", (e) => ws.send(e));
+  
+    ws.on("message", (data) => {
+        console.log(data.toString());
+        const path = './server/files/stat.txt';
+
+        if (data.toString() === "check") {
+            if (fs.existsSync(path)) {
+                var status =fs.readFileSync(path, 'utf8');
+            }
+            else {
+                var status = '0';
+            }
+            const message = JSON.stringify({
+                status: status,
+            });
+            ws.send(message);
+        }
+
+
+        if (data.toString() === "toggle") {
+            const start= new Date().getTime();
+                    
+            if (fs.existsSync(path)) {
+                //file exists
+                var status =fs.readFileSync(path, 'utf8');
+                if (status === '1') {
+                    fs.writeFileSync(path, '0', 'utf8');
+                }
+                else {
+                    fs.writeFileSync(path, '1', 'utf8');
+                }
+            }
+            else {
+                fs.writeFileSync(path, '1', 'utf8');
+            }
+            var status =fs.readFileSync(path, 'utf8');
+            console.log('status: ' + status);
+
+            const data = fs.readFileSync("server/files/file.rtf"); 
+            const stats = fs.statSync("server/files/file.rtf");
+            const size = stats.size;
+            console.log('fileSize: '+size);
+
+            file = data.toString();	 
+                    //console.log(file)
+            
+            const end = new Date().getTime();
+            var time = end - start;
+            console.log('Time: '+time);
+            const message = JSON.stringify({
+                status: status,
+                time: time,
+                size: size,
+                content: file.toString(), // здесь передаем данные в виде строки
+              });
+            ws.send(message);
+        };
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server runing on http://localhost:${PORT}`);
+});
+var htmlPath = path.resolve(__dirname, '..',  'client/index.html');
+app.get('/', function (req, res) {  
+      res
+      .status(200)
+        .sendFile(htmlPath);
+});
